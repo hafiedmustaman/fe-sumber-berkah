ca35b30a843e208889a24acbba926bb65eb9e2a8
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-12-02 09:34:56 +0700

    konfigurasi Global Function dengan Mixins

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+//define mixins for global function
+app.mixin({
+
+    methods: {
+
+        //money thousands
+        moneyFormat(number) {
+             let val = (number/1).toFixed(2).replace('.', ',')
+                return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+
+        },
+
+        //calculate discount
+        calculateDiscount(product) {
+            return product.price - (product.price *(product.discount)/100)
+        }
+
+    }
+})
+
+app.mount('#app')
