a24cd28c63ca74e501a46ab04caef8647674cbc8
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-12-02 15:34:16 +0700

    import Vue Router di main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//import router
+import router from './router'
+
 const app = createApp(App)
+
+//gunakan router di vue js dengan plugin "use"
+app.use(router)
 
 //define mixins for global function
 app.mixin({
