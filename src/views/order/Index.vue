1ba5101267a310c15de520077da7fa510772c1d3
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-12-04 04:48:29 +0700

    revisi membuat View Register dan Login

--- src/views/order/Index.vue
+++ src/views/order/Index.vue
@@ -9,16 +9,3 @@ export default {
     name: 'OrderIndexComponent'
 }
 </script>
-Dan masukkan kode berikut ini di dalam file Show.vue
-
-<template>
-    <div class="order-show">
-        HALAMAN ORDER DETAIL
-    </div>
-</template>
-
-<script>
-export default {
-    name: 'OrderShowComponent'
-}
-</script>
