d1be36c78223bd749b8bd8a0bddd4b6e867e5872
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-12-04 05:16:46 +0700

    import Module Order

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,15 @@ import { createStore } from 'vuex'
 //import module auth
 import auth from './module/auth'
 
+//import module order
+import order from './module/order'
+
 //create store vuex
 export default createStore({
 
     modules: {
         auth,       // <-- module auth
+        order,      // <-- module order
     }
 
 })
