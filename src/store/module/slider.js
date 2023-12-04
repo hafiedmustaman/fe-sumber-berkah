5c53fbcb8044ea9d6b540a8e143b7b1c5c251842
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-12-04 07:41:56 +0700

    edit Module Slider Vuex

--- src/store/module/slider.js
+++ src/store/module/slider.js
@@ -1,3 +1,6 @@
+//import global API
+import Api from '../../api/Api'
+
 const slider = {
 
     //set namespace true
@@ -5,16 +8,42 @@ const slider = {
 
     //state
     state: {
+
+        //index slider
+        sliders: []
     
     },
 
     //mutations
     mutations: {
 
+        //set state sliders dengan data dari response 
+        GET_SLIDERS(state, sliders) {
+            state.sliders = sliders
+        } 
+
     },
 
     //actions
     actions: {
+
+        //action getSliders
+        getSliders({ commit }) {
+
+            //get data sliders ke server
+            Api.get('/sliders')
+            .then(response => {
+
+                //commit ke mutation GET_SLIDERS dengan response data
+                commit('GET_SLIDERS', response.data.sliders)
+
+            }).catch(error => {
+
+                //show error log dari response
+                console.log(error)
+
+            })
+        }
 
     },
 
