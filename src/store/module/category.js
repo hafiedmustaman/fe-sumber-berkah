750c83cfa83ce6d9adf84938b8567c8634c5ee9f
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-12-04 07:37:25 +0700

    edit Module Category Vuex

--- src/store/module/category.js
+++ src/store/module/category.js
@@ -1,3 +1,6 @@
+//import global API
+import Api from '../../api/Api'
+
 const category = {
 
     //set namespace true
@@ -5,16 +8,42 @@ const category = {
 
     //state
     state: {
+
+        //index categories
+        categories: [],
     
     },
 
     //mutations
     mutations: {
 
+        //set state categories dengan data dari response 
+        GET_CATEGORIES(state, categories) {
+            state.categories = categories
+        },
+
     },
 
     //actions
     actions: {
+
+        //action getCategories
+        getCategories({ commit }) {
+
+            //get data categories ke server
+            Api.get('/categories')
+            .then(response => {
+
+                //commit ke mutation GET_CATEGORIES dengan response data
+                commit('GET_CATEGORIES', response.data.categories)
+
+            }).catch(error => {
+
+                //show error log dari response
+                console.log(error)
+
+            })
+        },
 
     },
 
