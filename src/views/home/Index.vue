c34204d1c409b3f619a7a4654bc24ef03c6f1ed5
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-12-04 07:46:33 +0700

    menampilkan Component Category dan Slider di View Home

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,11 +1,34 @@
 <template>
-    <div class="home">
-        HALAMAN HOME
+    <div>
+        <div class="container-fluid mt-3">
+            <div class="row">
+                <div class="col-md-3 mb-4">
+                    <Category /> <!-- component Category -->
+                </div>
+                <div class="col-md-9 mb-4">
+                    <Slider /> <!-- component Slider -->
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid mb-5 mt-4">
+            <!-- data product -->
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'HomeComponent',
-}
+    import Category from '../../components/Category.vue' // <-- component Category
+    import Slider from '../../components/Slider.vue' // <-- component Slider
+
+    export default {
+
+        name: 'HomeComponent',
+
+        components: {
+            Category,       // <-- register component Category
+            Slider          // <-- register component slider
+        }
+
+    }
 </script>
