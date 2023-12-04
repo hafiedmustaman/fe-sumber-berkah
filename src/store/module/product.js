b2a48c1be227676d838ebc8f9967433af42b3999
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-12-04 17:33:09 +0700

    edit Module Product Vuex

--- src/store/module/product.js
+++ src/store/module/product.js
@@ -1,3 +1,6 @@
+//import global API
+import Api from '../../api/Api'
+
 const product = {
 
     //set namespace true
@@ -6,15 +9,41 @@ const product = {
     //state
     state: {
     
+        //index products
+        products: [],
+
     },
 
     //mutations
     mutations: {
 
+        //set state products dengan data dari response 
+        GET_PRODUCTS(state, products) {
+            state.products = products
+        },
+
     },
 
     //actions
     actions: {
+
+        //action getProducts
+        getProducts({ commit }) {
+
+            //get data sliders ke server
+            Api.get('/products')
+            .then(response => {
+
+                //commit ke mutation GET_PRODUCTS dengan response data
+                commit('GET_PRODUCTS', response.data.products)
+
+            }).catch(error => {
+
+                //show error log dari response
+                console.log(error)
+
+            })
+        },
 
     },
 
