6001ee242aba5fe92e5e1df361c90ca8c429e9a0
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-12-06 07:19:59 +0700

    edit Module Auth Vuex

--- src/store/module/auth.js
+++ src/store/module/auth.js
@@ -110,25 +110,33 @@ const auth = {
             })
         },
 
-        //action logout
+        //fungsi logout
         logout({ commit }) {
-            
+                    
             //define callback promise
             return new Promise((resolve) => {
-            
+                    
                 //commit ke mutation AUTH_LOGOUT
                 commit('AUTH_LOGOUT')
-                
+        
                 //remove value dari localStorage
                 localStorage.removeItem('token')
                 localStorage.removeItem('user')
-
+        
+                /**
+                * commit ke module cart, untuk set mutation dan state cart menjadi kosong
+                */
+                commit('cart/GET_CART', 0, { root: true }) // <-- kita tambahkan root menjadi true, karena beda modulue
+                commit('cart/TOTAL_CART', 0, { root: true }) // <-- kita tambahkan root menjadi true, karena beda modulue
+        
+                //di atas kita set data-nya menjadi 0
+        
                 //delete header axios
                 delete Api.defaults.headers.common['Authorization']
-                
+        
                 //return resolve ke component 
                 resolve()
-
+        
             })
         },
 
@@ -137,46 +145,68 @@ const auth = {
 
             //define callback promise
             return new Promise((resolve, reject) => {
-            
-                Api.post('/login', {
                     
-                    //data yang dikirim ke server
-                    email: user.email,
-                    password: user.password,
-          
-                  })
-                    
-                  .then(response => {
-          
-                    //define variable dengan isi hasil response dari server
-                    const token = response.data.token
-                    const user  = response.data.user
-          
-                    //set localStorage untuk menyimpan token dan data user
-                    localStorage.setItem('token', token)
-                    localStorage.setItem('user', JSON.stringify(user))
-          
-                    //set default header axios dengan token
-                    Api.defaults.headers.common['Authorization'] = "Bearer " + token
+            Api.post('/login', {
+                            
+            //data yang dikirim ke server
+            email: user.email,
+            password: user.password,
+                
+            })
+                            
+            .then(response => {
+                
+            //define variable dengan isi hasil response dari server
+            const token = response.data.token
+            const user  = response.data.user
+                
+            //set localStorage untuk menyimpan token dan data user
+            localStorage.setItem('token', token)
+            localStorage.setItem('user', JSON.stringify(user))
+                
+            //set default header axios dengan token
+            Api.defaults.headers.common['Authorization'] = "Bearer " + token
 
-                    //commit auth success ke mutation
-                    commit('AUTH_SUCCESS', token, user)
+            //commit auth success ke mutation
+            commit('AUTH_SUCCESS', token, user)
 
-                    //commit get user ke mutation
-                    commit('GET_USER', user)
+            //commit get user ke mutation
+            commit('GET_USER', user)
 
-                    //resolve ke component dengan hasil response
-                    resolve(response)
-          
-                  }).catch(error => { 
-          
-                      //jika gagal, remove localStorage dengan key token
-                      localStorage.removeItem('token')
+            /**
+             * commit cart total dan cart count ke state yang ada di module cart
+             */
 
-                      //reject ke component dengan hasil response
-                      reject(error.response.data)
-          
-                  })
+            //get dat cart
+            Api.get('/cart')
+            .then(response => {
+                                
+                //commit mutation GET_CART
+                commit('cart/GET_CART', response.data.cart, { root: true }) // <-- kita tambahkan root menjadi true, karena beda modulue
+
+            })
+
+            //get total cart
+            Api.get('/cart/total')
+            .then(response => {
+                                
+                    //commit mutation TOTAL_CART
+                    commit('cart/TOTAL_CART', response.data.total, { root: true }) // <-- kita tambahkan root menjadi true, karena beda modulue
+
+            })
+
+            //resolve ke component dengan hasil response
+            resolve(response)
+                
+            }).catch(error => { 
+                
+                    //jika gagal, remove localStorage dengan key token
+                    localStorage.removeItem('token')
+
+                    //reject ke component dengan hasil response
+                    reject(error.response.data)
+                
+                })
 
             })
 
